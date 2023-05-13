85bbd3b16b01a1f9cb24eed6d0caaf1b52d4732a
jieunyuu <97153390+jieunyuu@users.noreply.github.com>
2023-05-14 01:07:56 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 748 ms, Memory: 298240 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        LinkedList<Integer> list=new LinkedList<>();
+        
+        for(int i=1;i<=n;i++) {
+        	list.add(i);
+        }
+        System.out.print("<");
+        
+        while(!list.isEmpty()) {
+        	for(int j=0;j<k;j++) {
+        		if(j==k-1) {
+        			int a=list.remove();
+        			if(list.size()==0) {
+        				System.out.print(a);
+        			}
+        			else
+        				System.out.print(a+", ");
+        		}
+        		else {
+        			list.add(list.remove());
+        		}
+        	}
+        }
+        System.out.print(">");
+    }
+}
+
+
