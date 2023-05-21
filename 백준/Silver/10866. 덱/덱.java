aa2f06c5cd15442866ae1039a048cb5b68472853
jieunyuu <97153390+jieunyuu@users.noreply.github.com>
2023-05-21 21:11:04 +0900

    덱.java

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -68,7 +68,6 @@ public class Main {
 		
 	
 	static void push_front(int val) {
-		// 원소를 먼저 넣은 뒤 front을 감소시킨다.
 		deque[front] = val;
 		front--;
 		size++;
@@ -85,10 +84,7 @@ public class Main {
 		if (size == 0) {
 			return -1;
 		} 
-		/*
-		 *  front + 1이 front 원소이므로 해당 원소를 임시 변수에 둔 뒤 
-		 *  front 을 +1 증가시킨다.
-		 */
+
 		int ret = deque[front + 1];
 		front++;
 		size--;	
@@ -99,6 +95,7 @@ public class Main {
 		if (size == 0) {
 			return -1;
 		} 
+		
 		int ret = deque[back];
 		back--;
 		size--;
