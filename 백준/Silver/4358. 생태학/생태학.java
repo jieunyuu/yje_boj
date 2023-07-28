fe6673922e6fa938cc8af01dc52a795a6388723c
jieunyuu <97153390+jieunyuu@users.noreply.github.com>
2023-07-28 23:58:41 +0900

    [Silver II] Title: 생태학, Time: 756 ms, Memory: 86704 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static HashMap<String, Integer> trees;
+	static int allCount;
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		trees = new HashMap<String, Integer>();
+		String str = br.readLine();
+		while(true) {
+            trees.put(str, trees.getOrDefault(str, 0) + 1);
+			allCount++;
+			
+			str = br.readLine();
+			if(str == null || str.length() == 0) {
+				break;
+			}
+		}
+		
+		// 키 오름차순 정렬 
+		Object[] keys = trees.keySet().toArray();
+		Arrays.sort(keys);
+		
+		StringBuilder sb = new StringBuilder();
+		for(Object key : keys) {
+			String keyStr = (String) key;
+			int count = trees.get(keyStr);
+			double per = (double)(count * 100.0) / allCount;
+			
+			sb.append(keyStr + " " + String.format("%.4f", per) + "\n");	// 소수점 4번 째 자리까지 출력 
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
